eae9fca16c809f9f440ec84bec27538336777923
cristopher-gomez-m <58451312+cristopher-gomez-m@users.noreply.github.com>
2021-02-06 22:02:21 -0500

    se agrego el comando start con un mensaje de bienvenida

--- Rotu_Go_bot.py
+++ Rotu_Go_bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
-from telegram.ext import Updater
+import telegram
+from telegram.ext import Updater,CommandHandler
 
 #Configurar el loggin
 logging.basicConfig(
@@ -11,4 +12,24 @@ logger = logging.getLogger()
 
 #Solicitar el TOKEN
 TOKEN= os.getenv("TOKEN")
-print(TOKEN)
+
+def start(update, context):
+    logger.info(f"El usuario {update.effective_user['fist_name']},ha iniciado una conversación")
+    name= update.effective_user['first_name']
+    update.message.reply_text(f"Hola {name}, soy tu asistente de vuelo UwU")
+
+if __name__ == "__main__":
+    #Obtenemos la informacion del bot
+    bot= telegram.Bot(token= TOKEN)
+    print(bot.getMe())
+
+#Enlazamos el updater con el bot
+updater= Updater(bot.token, use_context=True)
+
+#Despachador
+despachador = updater.dispatcher
+
+#Creamos las funciones del bot
+despachador.add_handler(CommandHandler("start", start))
+updater.start_polling()
+updater.idle() #Permite finaliza el bot con Ctrl + C
