e25e7edbf9906847700f953036dd7c15ad6fe9f1
cristopher-gomez-m <58451312+cristopher-gomez-m@users.noreply.github.com>
2021-02-14 14:52:15 -0500

    conectada la base de datos con el programa,arreglada la funcion listar

--- Rotu_Go_bot.py
+++ Rotu_Go_bot.py
@@ -27,10 +27,8 @@ def random_number(update, context):
     context.bot.sendMessage(chat_id=user_id,parse_mode="HTML", text=f"Numero aleatorio:\n{number}")
 #Listar todos los vuelos disponibles. 
 def listar(update, context):
-    logger.info(f"El usuario {update.effective_user['first_name']},ha iniciado una conversación")
-    name= update.effective_user['first_name']
-    update.message.reply_text(f"Hola {name}-kun, soy tu asistente de vuelo,¿que deseas hacer? UwU ")
-    
+    user_id= update.effective_user['id']
+    logger.info(f"El usuario {user_id},ha solicitado listar los vuelos")
     conn = psycopg2.connect(
         dbname="Rotu_Go_bot",
         user="postgres",
@@ -39,13 +37,12 @@ def listar(update, context):
         port="5432"
     )
     cursor=conn.cursor()
-    query= '''SELECT id_vuelo, codigo_iata, aeropuerto, lugar_de_origen, lugar_de_llegada, asientos, fecha_de_ida, fecha_de_llegada
-	FROM public.vuelo'''
+    query= '''SELECT aeropuerto, codigo_iata,lugar_de_origen,lugar_de_llegada,fecha_de_ida, fecha_de_llegada FROM vuelo'''
     cursor.execute(query)
-    update.message.reply_text(query)
-    row= cursor.fetchAll()
+    row= cursor.fetchall()
+    #print(row[0][1]) {}
     for x in row:
-        update.message.reply_text("ASD")
+        context.bot.sendMessage(chat_id=user_id,parse_mode="HTML", text=f"Vuelo:\n{x[0]},{x[1]},{x[2]},{x[3]},{x[4]},{x[5]}")
 
     conn.commit()
     conn.close()
@@ -78,14 +75,20 @@ despachador = updater.dispatcher
 
 #Creamos las funciones del bot
 despachador.add_handler(CommandHandler("start", start))
-updater.start_polling()
-updater.idle() #Permite finaliza el bot con Ctrl + C
 despachador.add_handler(CommandHandler("listar",listar))
+despachador.add_handler(CommandHandler("random",random_number))
 despachador.add_handler(CommandHandler("SEARCHD",SEARCHD))
 despachador.add_handler(CommandHandler("SEARCHO",SEARCHO))
 despachador.add_handler(CommandHandler("BUY_TICKET",BUY_TICKET))
 despachador.add_handler(CommandHandler("BUYRT_TICKET",BUYRT_TICKET))
-despachador.add_handler(CommandHandler("random",random_number))
+updater.start_polling()
+updater.idle() #Permite finaliza el bot con Ctrl + C
+updater.start_polling()
+despachador.add_handler(CommandHandler("SEARCHD",SEARCHD))
+despachador.add_handler(CommandHandler("SEARCHO",SEARCHO))
+despachador.add_handler(CommandHandler("BUY_TICKET",BUY_TICKET))
+despachador.add_handler(CommandHandler("BUYRT_TICKET",BUYRT_TICKET))
+
 
 
  
